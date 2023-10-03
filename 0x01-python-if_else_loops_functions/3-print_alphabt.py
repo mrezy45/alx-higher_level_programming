67c23c6f3342f611c1c127df344ce02b484f2669
Ezra Rodney Mpiima <mrezy45@gmail.com>
2023-10-03 14:50:02 -0700

    0x01. Python - if/else, loops, functions tasks

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) == 'q' or chr(i) == 'e':
-        continue
-    print(chr(i).format(i),end='')
+for alphabet in range(97, 123):
+    if chr(alphabet) != 'q' and chr(alphabet) != 'e':
+        print("{}".format(chr(alphabet)), end="")
