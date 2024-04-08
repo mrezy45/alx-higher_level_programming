dc02965ad65160d789c2ce8ad2ae05bcc10bdd6f
Ezra Rodney Mpiima <mrezy45@gmail.com>
2024-04-08 12:44:38 -0700

    0x12. JavaScript - Warm up tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,5 +2,5 @@
 const myVar = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 
 myVar.forEach((element) => {
-	console.log(element);
+  console.log(element);
 });
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,9 +2,9 @@
 const x = process.argv[2];
 
 if (!parseInt(x)) {
-	console.log('Missing number of occurences');
+  console.log('Missing number of occurences');
 } else {
-	for (let i = 0; i < x; i++) {
-		console.log('C is fun');
-	}
+  for (let i = 0; i < x; i++) {
+    console.log('C is fun');
+  }
 }
