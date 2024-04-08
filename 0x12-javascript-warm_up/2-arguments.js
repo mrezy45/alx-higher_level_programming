90dffb803c113f568bc84a4083c0127aa3c76e91
Ezra Rodney Mpiima <mrezy45@gmail.com>
2024-04-08 13:18:57 -0700

    0x12. JavaScript - Warm up tasks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,5 +4,5 @@ const argc = process.argv.length;
 if (argc > 2) {
   console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
 } else {
-  console.log('No Argument');
+  console.log('No argument');
 }
